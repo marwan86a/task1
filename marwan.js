818dead1b4d06981920329808c87339dddf5859e
marwan86a <161903920+marwan86a@users.noreply.github.com>
2024-08-21 00:20:00 +0300

    Add files via upload

---
+++ marwan.js
@@ -0,0 +1,21 @@
+var name="marwan elsyed"
+
+var title="mansoura"
+var phone ="010154242"
+console.log( name , title, phone)
+console.log(typeof phone )
+
+let x = 500;
+function hello() {
+  x = 100;
+}
+console.log(x);
+
+
+let o=23
+function hello(){
+   o=100;
+ console.log(o)
+}
+hello();
+console.log(o);
